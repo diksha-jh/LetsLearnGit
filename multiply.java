cdb94f3cd42caa631b214f617e7d30b70bb2f0fb
Diksha <inglediksha97@gmail.com>
2023-06-21 14:06:48 +0530

    multiply commit

---
+++ multiply.java
@@ -0,0 +1,11 @@
+public class multiply{
+    public static vid main(String[] args){
+        int a = 4;
+        int b = 6;
+        System.out.println(a*b);
+        System.out.println("..");
+
+    }
+    
+
+}
